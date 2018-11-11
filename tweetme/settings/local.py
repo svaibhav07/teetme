bca034ba8b4b225f893eb4d1129826b08dac5158
svaibhav07 <33864787+svaibhav07@users.noreply.github.com>
2018-11-11 17:25:45 +0530

    Update local.py

--- tweetme/settings/local.py
+++ tweetme/settings/local.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(^)xpu7m-5-2vm$6h&4t$g2$2!r^5s5**hbsuueb-2lo_61+2f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
